ca8c6b4b19040d93d38c283407f43d16589d2381
Zonaro <zonaro@outlook.com>
2024-11-29 15:13:40 -0300

    Altera operador de comparação para verificar método POST no arquivo default.php

--- default.php
+++ default.php
@@ -4,7 +4,7 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Connection-String, Response-Type');
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     try {
         header('Content-Type: application/json');
         $headers = getallheaders();
