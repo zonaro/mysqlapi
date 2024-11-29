0f358aa091622c4b8a6e25163e4942e20673e8a8
Zonaro <zonaro@outlook.com>
2024-11-29 10:28:25 -0300

    Adiciona cabeçalhos CORS para permitir requisições de diferentes origens

--- default.php
+++ default.php
@@ -1,5 +1,9 @@
 <?php
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Connection-String, Response-Type');
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     try {
         header('Content-Type: application/json');
